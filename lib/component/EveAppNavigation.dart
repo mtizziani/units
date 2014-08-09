ca52b02e3dc6c328e16dc48f1445b53d76e13fce
Maik Tizziani <mtizziani@gmail.com>
2014-08-09 12:53:30 +0200

    added logic for navigation points added .gitignore file

--- lib/component/EveAppNavigation.dart
+++ lib/component/EveAppNavigation.dart
@@ -9,6 +9,7 @@ class EveAppNavigation {
   bool _debug;
   EveAppCtrl _ctrl;
   String _title;
+  HashMap<String, List> _navEntries;
 
   get title => _title;
   set title(String value){
@@ -29,9 +30,20 @@ class EveAppNavigation {
       _debug = EveAppConfig.statusDebugNavigation;
     }
     title = _ctrl.title + " - Navigation";
+    initNavigation();
   }
 
   void titleToCtrl(String value){
     _ctrl.title = value;
   }
+
+  void initNavigation(){
+    _navEntries.putIfAbsent('de', new List<String>());
+    _navEntries.putIfAbsent('en', new List<String>());
+  }
+
+  void putToNavigationList(String language, String entry){
+    _navEntries[language].add(entry);
+  }
+  List<String> getNavigationList(String language) => _navEntries[language];
 }
